d0b296e0553268427c446591e629509e10cb6cfd
linneacfw <lifr9411@colorado.edu>
2023-11-01 19:14:16 +0000

    fixed return statement

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def hello_world():
     return 'Hello, World!'
 
 @app.route('/db_test')
-def db_test():
+def testing():
     conn = psycopg2.connect("postgres://lfw_lab10db_user:YU0QBOASi64a4TKGi6Z434YDgCYNWXKA@dpg-cl19te2s1bgc73ekto30-a/lfw_lab10db")
     conn.close()
-    return("Successful Database Connection")
+    return "Successful Database Connection"
